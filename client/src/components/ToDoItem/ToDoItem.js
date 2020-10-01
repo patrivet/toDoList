84a5e7ca35e96161071c89a8cd0699bf2370f166
Pat Rivet <patrickmrivet@gmail.com>
2020-10-01 14:29:33 +0100

    (chore) remove test console logs

--- client/src/components/ToDoItem/ToDoItem.js
+++ client/src/components/ToDoItem/ToDoItem.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import store from '../../store';
 import { toggleToDo } from '../../actions/actions';
-
 import './ToDoItem.css';
-console.log('INFO: ToDo ITEM..');
 
 const ToDoItem = ({ toDo }) => {
-  const handleToDoClick = e => {
-    console.log('Hi from ToDo click ');
-    // ...? Dispatch toggle action
+  const handleToDoClick = () => {
+    // Dispatch toggle action
     store.dispatch(toggleToDo(toDo.id));
   };
 
--- client/src/containers/ToDoList/ToDoList.js
+++ client/src/containers/ToDoList/ToDoList.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import ToDoItem from '../../components/ToDoItem';
 import './ToDoList.css';
-console.log('INFO: ...ToDo LIST..');
-
 let nextId = 0;
 
 const ToDoList = () => {
   // Get the toDos from the store.
   const toDos = useSelector(state => state.toDos);
-  console.log('ToDoList -> toDos', toDos);
   return toDos.map(toDoItem => {
     return <ToDoItem key={++nextId} toDo={toDoItem} />;
   });
