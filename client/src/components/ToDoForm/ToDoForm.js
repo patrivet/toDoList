21532db5db12141a917295fb1972d4f1becb52f8
Pat Rivet <patrickmrivet@gmail.com>
2020-10-03 13:55:53 +0100

    (fix) duplicate toDo does not get added

--- client/src/components/ToDoForm/ToDoForm.js
+++ client/src/components/ToDoForm/ToDoForm.js
@@ -8,8 +8,10 @@ const ToDoForm = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    // If there is an toDo entered - addToDo
-    // pass an action (from the predefined actions)
+    // Get entry in the toDo input
+    toDo = document.getElementById('description_input').value;
+
+    // If there is an toDo entered - dispatch the addToDo action
     if (toDo) {
       store.dispatch(addToDo(toDo));
       // clear the content from the input element & clear the toDo local var.
@@ -18,15 +20,16 @@ const ToDoForm = () => {
     }
   };
 
-  const handleToDo = event => {
-    const { value } = event.target;
-    // If there is an toDo entered -store here.
-    toDo = value;
-  };
+  // Currently not being used as onChange on input wasn't firing for a duplicate value.
+  // const handleToDo = event => {
+  //   const { value } = event.target;
+  //   // If there is an toDo entered -store here.
+  //   toDo = value;
+  // };
 
   return (
     <form onSubmit={handleSubmit} id="toDoForm">
-      <input id="myInput" type="text" name="toDo" onChange={handleToDo}></input>
+      <input id="description_input" type="text" name="toDo"></input>
       <button type="submit">+</button>
     </form>
   );
