789cd9d0da1d9eebc0af701fbdea72b04eb6118b
Pat Rivet <patrickmrivet@gmail.com>
2020-10-09 15:54:46 +0100

    (refactor) Search functionality from own design to using material-ui (use textfield with built in clear function)

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -2,54 +2,25 @@ import React, { useState } from 'react';
 import { searchToDos } from '../../actions';
 import store from '../../store';
 import './Search.css';
-import clearImg from '../../imgs/clear.png';
+//import HighlightOffRoundedIcon from '@material-ui/icons/HighlightOffRounded';
+import TextField from '@material-ui/core/TextField';
 
 const Search = () => {
-  const [resetSearch, setResetSearch] = useState('');
-  const handleFormSubmit = e => {
-    e.preventDefault();
-    handleSearch();
-  };
-
-  const clearForm = () => {
-    // Clear the entire form (the search text)
-    document.getElementById('searchForm').reset();
-    setResetSearch('');
-    handleSearch();
-  };
-
-  const handleSearch = () => {
-    console.log('SEARCH: handleSearch()');
-    // Dispatch search action
-    // get input value
-    const searchVal = document.getElementById('searchForm__input').value;
-    store.dispatch(searchToDos(searchVal));
-  };
-
   const handleSearchInput = e => {
     // Store the search string and search ??
     const { name, value } = e.target;
-    setResetSearch(value ? 'show' : '');
     store.dispatch(searchToDos(value));
   };
 
   return (
-    <div>
-      <form id="searchForm" onSubmit={handleFormSubmit}>
-        <input
-          id="searchForm__input"
-          name="searchForm__input"
-          onKeyUp={handleSearchInput}
-          placeholder="🔍  Enter search..."
-        ></input>
-        <button type="submit">search here</button>
-
-        <img
-          className={`searchForm__reset ${resetSearch}`}
-          src={clearImg}
-          onClick={clearForm}
-        ></img>
-      </form>
+    <div className="outlined-search-con">
+      <TextField
+        id="outlined-search"
+        label="Search"
+        type="search"
+        variant="outlined"
+        onChange={handleSearchInput}
+      />
     </div>
   );
 };
