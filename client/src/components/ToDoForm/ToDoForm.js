85eb9f47223c06c4160706098403de647e23cb0e
Pat Rivet <patrickmrivet@gmail.com>
2020-10-13 18:50:44 +0100

    Add dark / light theme CSS variables.  Remove toDoForm dashed border.

--- client/src/components/ToDoForm/ToDoForm.js
+++ client/src/components/ToDoForm/ToDoForm.js
@@ -7,6 +7,49 @@ import TextField from '@material-ui/core/TextField';
 import CreateTwoTone from '@material-ui/icons/CreateTwoTone';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
+import { withStyles } from '@material-ui/core/styles';
+
+// Define materialUI colors for input (text) field
+let isDarkThemeOn = true;
+
+// Light theme (postfix __L)
+let focus_color__L = '#BDBDBD'; // light grey
+let border_color__L = 'black';
+let click_color__L = '#3F51B5'; // blue
+
+// Dark theme colors (postfix __D)
+let focus_color__D = 'white';
+let border_color__D = '#BDBDBD';
+let click_color__D = 'white';
+
+// Set color based on darkTheme on or off.
+let focus_color = isDarkThemeOn ? focus_color__D : focus_color__L;
+let border_color = isDarkThemeOn ? border_color__D : border_color__L;
+let click_color = isDarkThemeOn ? click_color__D : click_color__L;
+
+const CssTextField = withStyles({
+  root: {
+    // NOT USED HERE
+    // '& label.Mui-focused': {
+    //   color: focus_color,
+    // },
+    // '& .MuiInput-underline:after': {
+    //   borderBottomColor: 'green',
+    // },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: border_color,
+      },
+      '&:hover fieldset': {
+        borderColor: focus_color,
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: click_color,
+      },
+    },
+  },
+})(TextField);
+
 const ToDoForm = () => {
   let toDo;
 
@@ -33,8 +76,7 @@ const ToDoForm = () => {
 
   return (
     <form onSubmit={handleSubmit} id="toDoForm">
-      {/* <input id="description_input" type="text" name="toDo"></input> */}
-      <TextField
+      <CssTextField
         id="description_input"
         size="small"
         variant="outlined"
@@ -46,7 +88,7 @@ const ToDoForm = () => {
             </InputAdornment>
           ),
         }}
-      ></TextField>
+      ></CssTextField>
 
       <Button
         className="description_submit"
