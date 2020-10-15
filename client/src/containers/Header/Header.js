c4624090e69732004746ebfd02ea467b30b21e31
Pat Rivet <patrickmrivet@gmail.com>
2020-10-15 16:35:10 +0100

    Add header label

--- client/src/containers/Header/Header.js
+++ client/src/containers/Header/Header.js
@@ -6,7 +6,10 @@ import toDoIcon from '../../imgs/todo-list-48.png';
 const Header = () => {
   return (
     <div className="header">
-      <img className="header__icon" src={toDoIcon} />
+      <div className="header__iconAndTitle">
+        <img className="header__icon" src={toDoIcon} />
+        <h1 className="header__title">To Do List</h1>
+      </div>
       <div className="header__search">
         <Search />
       </div>
