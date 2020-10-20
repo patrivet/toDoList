4b3cac6e33daec3105c86d7acd397ebeed4b0579
Pat Rivet <patrickmrivet@gmail.com>
2020-10-20 16:53:27 +0100

    Add redux-logger package (enabled with default logging)

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducers/toDos';
+// Logger with default options
+import logger from 'redux-logger';
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__() &&
+    applyMiddleware(logger)
 );
 
 export default store;
