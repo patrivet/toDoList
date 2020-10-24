1a5e934e26eb1d7b15bb94e7fb9a0520ea7b01a2
Pat Rivet <patrickmrivet@gmail.com>
2020-10-24 16:57:15 +0100

    (refactor) toDo list icon color for light/dark theme

--- client/src/containers/Header/Header.js
+++ client/src/containers/Header/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Search from '../../components/Search';
+import { useSelector } from 'react-redux';
 import './Header.css';
-import toDoIcon from '../../imgs/todo-list-48.png';
+import toDoIconLight from '../../imgs/todo-list-48-light.png';
+import toDoIconDark from '../../imgs/todo-list-48-dark.png';
 
 const Header = () => {
+  const isLightThemeOn = useSelector(state => state.isLightThemeOn);
+  console.log("Header -> isLightThemeOn", isLightThemeOn)
   return (
     <div className="header">
       <div className="header__iconAndTitle">
-        <img className="header__icon" src={toDoIcon} alt="toDoList_icon" />
+        <img className="header__icon" src={isLightThemeOn ? toDoIconDark : toDoIconLight} alt="toDoList_icon" />
         <h1 className="header__title">To Do List</h1>
       </div>
       <div className="header__search">
